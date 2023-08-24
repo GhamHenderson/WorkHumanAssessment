530b2cc185b6bf09408fb83fbe18bc7f66ac161f
Graham Henderson <62728935+GhamHenderson@users.noreply.github.com>
2023-08-24 18:11:44 +0100

    Initial Upload
    Flask, React App chosen.
    MySQL Docker image now returns JSON list of countries.
    
    Flask Start done,
    React start done.

---
+++ flask-server/server.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Resource, Api
+import json
+import requests
+import mysql.connector
+import sys, os
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route("/")
+def index():
+    return render_template('./frontend/public/index.html')
+
+
+@app.route("/country")
+def country():
+    # Create an instance of the getAllData resource
+    get_all_data_resource = getAllData()
+    # Call the get method to retrieve data
+    data = get_all_data_resource.get()
+    return jsonify(data)
+
+
+# Get Country Name and Index from MySQL DB
+class getAllData(Resource):
+    def get(self):
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="example",
+            port=2222,
+            database='workhumandb1'
+        )
+
+        buffer = []
+        mycursor = mydb.cursor()
+        mycursor.execute('SELECT * FROM countries;')
+        data = mycursor.fetchall()
+
+        for row in data:
+            id = row[0]
+            country_name = row[1]
+            # Create a dictionary for each row and append it to the buffer list
+            row_dict = {'id': id, 'country': country_name}
+            buffer.append(row_dict)
+
+        mycursor.close()
+        mydb.close()
+
+        # Convert the buffer list to JSON
+        json_data = json.dumps(buffer)
+        parsed_json_data = json.loads(json_data)
+        return parsed_json_data
+
+
+api.add_resource(getAllData, '/getAllData')
+
+if __name__ == '__main__':
+    app.run(debug=True)
